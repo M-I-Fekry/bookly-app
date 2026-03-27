7e06222966041f12494c7a47b7ed2ebed9a69edb
Fekry <me031230@gmail.com>
2026-03-27 07:14:45 +0200

    feat: implement CustomAppBar for HomeView with logo and search icon.

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 45),
+      child: Row(
+        children: [
+          Image.asset(AssetsData.logo, height: 28),
+          const Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              CupertinoIcons.search,
+              size: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
