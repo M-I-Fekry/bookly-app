288e4d2c9c65b773c53af8430d44e5a110e23a62
Fekry <me031230@gmail.com>
2026-03-31 07:07:45 +0200

    feat: add best seller book card widget.

---
+++ lib/features/home/presentation/views/widgets/best_seller_book_card.dart
@@ -0,0 +1,48 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerBookCard extends StatelessWidget {
+  const BestSellerBookCard({super.key, required this.imageUrl});
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Image.asset(
+            imageUrl,
+            width: 65,
+            height: 90,
+            fit: BoxFit.cover,
+          ),
+        ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Harry Potter\nand the Goblet of Fire',
+                style: Styles.textStyle15,
+              ),
+              const SizedBox(height: 4),
+              const Text('J.K. Rowling', style: Styles.textStyle13),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  const Text('19.99 €', style: Styles.textStyle14),
+                  const Spacer(),
+                  const Icon(Icons.star, color: Colors.amber, size: 16),
+                  const SizedBox(width: 4),
+                  const Text('4.8 (2390)', style: Styles.textStyle12),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
