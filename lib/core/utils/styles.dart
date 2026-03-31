e036fbf9a06c33e83ff6f84c40e9de7a0fc61917
Fekry <me031230@gmail.com>
2026-03-31 07:02:27 +0200

    feat: add app text styles.

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const textStyle12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+    color: Colors.grey,
+  );
+
+  static const textStyle13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.normal,
+    color: Colors.grey,
+  );
+
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const textStyle15 = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    height: 1.3,
+  );
+
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+}
