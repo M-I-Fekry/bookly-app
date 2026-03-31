434670e600269a925d42c8343535ba5835e80ca1
Fekry <me031230@gmail.com>
2026-03-31 07:04:44 +0200

    feat: add books images assets.

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,13 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
+  static const book1 = 'assets/images/book1.png';
+  static const book2 = 'assets/images/book2.png';
+  static const book3 = 'assets/images/book3.png';
+  static const book4 = 'assets/images/book4.png';
+  static const book5 = 'assets/images/book5.png';
+  static const book6 = 'assets/images/book6.png';
+  static const book7 = 'assets/images/book7.png';
+  static const book8 = 'assets/images/book8.png';
+  static const book9 = 'assets/images/book9.png';
+  static const book10 = 'assets/images/book10.png';
 }
