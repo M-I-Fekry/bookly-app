41f552f490b00489b1d3833e4dc8720e2c8ecd80
Fekry <me031230@gmail.com>
2026-04-12 09:20:52 +0200

    feat: add book details action buttons widget.

---
+++ lib/features/home/presentation/views/widgets/book_details_action_buttons.dart
@@ -0,0 +1,39 @@
+import 'package:bookly_app/core/utils/custom_button.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsActionButtons extends StatelessWidget {
+  const BookDetailsActionButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Row(
+        children: [
+          Expanded(
+            child: CustomButton(
+              text: '19.99€',
+              backgroundColor: Colors.white,
+              textColor: Colors.black,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+              ),
+            ),
+          ),
+          Expanded(
+            child: CustomButton(
+              text: 'Free preview',
+              backgroundColor: const Color(0xFFEF8262),
+              textColor: Colors.white,
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
