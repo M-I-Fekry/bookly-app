7693b32884b380774600995a8539f7d789d1bcd6
Fekry <me031230@gmail.com>
2026-04-12 09:20:16 +0200

    feat: add book details app bar widget.

---
+++ lib/features/home/presentation/views/widgets/book_details_app_bar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class BookDetailsAppBar extends StatelessWidget {
+  const BookDetailsAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(Icons.close, color: Colors.white, size: 28),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart_outlined,
+                color: Colors.white, size: 28),
+          ),
+        ],
+      ),
+    );
+  }
+}
