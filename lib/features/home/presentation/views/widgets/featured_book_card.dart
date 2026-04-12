bc4dcb9cf1dcbf0b81372b0326ae855fb421b6e5
Fekry <me031230@gmail.com>
2026-04-12 09:22:52 +0200

    refactor: update featured book card navigation to go router.

--- lib/features/home/presentation/views/widgets/featured_book_card.dart
+++ lib/features/home/presentation/views/widgets/featured_book_card.dart
@@ -1,14 +1,21 @@
+import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBookCard extends StatelessWidget {
-  const FeaturedBookCard({super.key, required this.imageUrl});
   final String imageUrl;
+  const FeaturedBookCard({super.key, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(16),
-      child: Image.asset(imageUrl, width: 130, height: 200, fit: BoxFit.cover),
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const BookDetailsView()),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Image.asset(imageUrl, width: 130, height: 200, fit: BoxFit.cover),
+      ),
     );
   }
 }
