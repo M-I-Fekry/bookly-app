7162acc8f8a379505471c45ca404f4719f87ffe2
Fekry <me031230@gmail.com>
2026-04-12 09:19:19 +0200

    feat: update custom app bar to navigate to search.

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -14,7 +16,9 @@ class CustomAppBar extends StatelessWidget {
           Image.asset(AssetsData.logo, height: 28),
           const Spacer(),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            },
             icon: const Icon(
               CupertinoIcons.search,
               size: 30,
