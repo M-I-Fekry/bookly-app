b3eddbbee45fb5d12ee4cce0df55c6142b79ad6b
Fekry <me031230@gmail.com>
2026-04-12 09:18:12 +0200

    feat: add app router with go router.

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,52 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/search/presentation/views/search_view.dart';
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
+
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) => const SearchView(),
+      ),
+      GoRoute(
+        path: kHomeView,
+        pageBuilder: (context, state) => CustomTransitionPage(
+          child: const HomeView(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+          transitionDuration: kTranstionDuration,
+        ),
+      ),
+      GoRoute(
+        path: kBookDetailsView,
+        pageBuilder: (context, state) => CustomTransitionPage(
+          child: const BookDetailsView(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+          transitionDuration: kTranstionDuration,
+        ),
+      ),
+    ],
+  );
+}
